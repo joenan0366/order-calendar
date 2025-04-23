328d488b510f3190d17645bf5a7f6f08e36b6031
eis1008 <sanuki_udon@live.jp>
2025-04-23 11:50:26 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("https://joenan.site/wp-json/order/v1/login", {
+      const response = await fetch("https://joenan.sadist.jp/wordpress/wp-json/order/v1/login", {
         method: "POST",
         body: JSON.stringify({
           type: "login",
@@ -92,7 +92,7 @@ function App() {
     };
   
     try {
-      const response = await fetch("https://joenan.site/wp-json/order/v1/submit", {
+      const response = await fetch("https://joenan.sadist.jp/wordpress/wp-json/order/v1/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
